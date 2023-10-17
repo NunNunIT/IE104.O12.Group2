7a32c69696eef9e1c9b36c519c2a11c4556a1984
Huong Vi <vithihuong384@gmail.com>
2023-10-18 02:28:12 +0700

    add src/views/components/slider.ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@
 const siteRouter = require('./siteRouter')
 
 function route(app) {
+    app.use('/slider', (req, res)=> {
+        res.render('./components/slider')
+    })
     app.use('/', siteRouter)
 }
 
