b78e6a4bef46e811008de569e1364f21e0d123f7
namtuthien <leducmanh123kt@gmail.com>
2023-10-17 15:49:49 +0700

    Add src/views/partials
    Add src/views/pages/index.ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,20 @@
 const siteRouter = require('./siteRouter')
 
 function route(app) {
+    // test product
+    app.use('/', (req, res) => {
+        const productList = {
+            'cooker': 'Nồi cơm điện',
+            'fan': 'Quạt điện',
+            'fridge': 'Tủ lạnh',
+            'television': 'Ti vi',
+            'water-purifier': 'Máy lọc nước',
+            'induction-cooker': 'Bếp từ'
+        }
+
+        res.render('./pages/index', { productList })
+    })
+
     app.use('/', siteRouter)
 }
 
