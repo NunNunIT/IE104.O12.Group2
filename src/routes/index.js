2945638bb55e3bb2d5175858b1f3df75744fe82a
LY PHI LAN <21520319@gm.uit.edu.vn>
2023-10-26 22:37:01 +0700

    add /scr/partials/app-bar.ejs, /scr/style/partials/app-bar.css

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,12 @@ const productRouter = require('./productRouter')
 
 function route(app) {
 
+    app.use('/appbar', (req, res) => { res.render('./partials/app-bar.ejs') })
+    app.use('/footer', (req, res) => { res.render('./partials/footer.ejs') })
+
     app.use('/product', productRouter)
     app.use('/', siteRouter)
+    
 
 }
 
