09aede819f7de5782780288587406a4a1322d910
Huong Vi <vithihuong384@gmail.com>
2023-11-04 01:23:02 +0700

    update  src/public/styles/components/slider.css, add src/views/pages/login.ejs, add user_account

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,3 +1,8 @@
+const db = require('../config/db/connect');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { promisify } = require("util");
+
 class SiteController {
     // [GET] /
     index(req, res) {
@@ -12,6 +17,33 @@ class SiteController {
 
         res.render('./pages/index', { productCatalog })
     }
+
+    // [GET] /login
+    login(req, res) {
+        res.render('./pages/login')
+    }
+
+    // [POST] /login
+    submitLogin(req, res) {
+        const { user_phone, user_password } = req.body;
+        db.query('SELECT *  FROM users WHERE user_phone = ?', [user_phone], async (err, result) => {
+            console.log(result);
+            if (err) throw err;
+            // if (!result.length || !await bcrypt.compare(user_password, result[0].user_password)) return res.json({
+            if (!result[0]) return res.json({
+                status: "error",
+                error: "Số điện thoại không tồn tại."
+            })
+            else if ((user_password !== result[0].user_password)) return res.json({
+                status: "error2",
+                error: "Mật khẩu không chính xác."
+            })
+        })
+
+    }
+
+
+
 }
 
 module.exports = new SiteController()
--- src/routes/siteRouter.js
+++ src/routes/siteRouter.js
@@ -4,6 +4,8 @@ const router = express.Router();
 // import controller
 const SiteController = require('../controllers/SiteController.js')
 
+router.get('/login', SiteController.login)
+router.post('/login', SiteController.submitLogin)
 router.get('/', SiteController.index)
 
 module.exports = router
