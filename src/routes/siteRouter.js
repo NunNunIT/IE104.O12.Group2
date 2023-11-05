3194af44206e5554c1e4d87c46e76b7f7c5efed1
LY PHI LAN <21520319@gm.uit.edu.vn>
2023-11-05 23:03:27 +0700

    add publics/pages/trasaction.css pages/transaction.ejs

--- src/routes/siteRouter.js
+++ src/routes/siteRouter.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const SiteController = require('../controllers/SiteController.js')
 
 router.get('/product-detail', SiteController.productDetail)
-
+router.get('/forgot-password', SiteController.forgotPassword)
+router.get('/reset-password', SiteController.resetPassword)
+router.get('/transaction', SiteController.transaction)
 router.get('/', SiteController.index)
 
 module.exports = router
