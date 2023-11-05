d0f63f8bec5a05a83b9799d8feec1d8301880af1
namtuthien <leducmanh123kt@gmail.com>
2023-11-05 22:17:18 +0700

    Add /pages/cart, /components/cart-product

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const productRouter = require('./productRouter')
 const notificationsRouter = require('./notificationsRouter')
 
 function route(app) {
+    app.use('/cart', (req, res) => res.render('./pages/cart'))
     app.use('/notifications', notificationsRouter)
     app.use('/product', productRouter)
     app.use('/', siteRouter)
