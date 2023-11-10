90ed106f28d9cb8a3242238f43ed10ebbb024554
namtuthien <leducmanh123kt@gmail.com>
2023-11-10 17:15:29 +0700

    Add /pages/order-information

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const productRouter = require('./productRouter')
 const notificationsRouter = require('./notificationsRouter')
 
 function route(app) {
+    app.use('/order-info', (req, res) => res.render('./pages/order-information'))
     app.use('/cart', (req, res) => res.render('./pages/cart'))
     app.use('/notifications', notificationsRouter)
     app.use('/product', productRouter)
