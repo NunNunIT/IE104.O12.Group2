dbda78fcb9550c5983310bb0f6ca7e7b674f942d
[Demo] <[pnhaiyen520@gmail.com]>
2023-11-17 15:01:39 +0700

    Fix /partials/header, partials/footer, /pages/about-us, /pages/privacy-policy, /pages/notification

---
+++ src/controllers/TestController.js
@@ -0,0 +1,12 @@
+const test = require('../models/test.model')
+
+class TestController {
+    detail(req, res) {
+        test.getDetail(req, res, (err, results) => {
+            // res.send({ product_data: results, })
+            res.status(200).render('./pages/search/detail', { product_data: results, })
+        })
+    }
+}
+
+module.exports = new TestController()
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,11 @@ const accountRouter = require('./accountRouter')
 const authRouter = require('./authRouter')
 const orderRouter = require('./orderRouter')
 const searchRouter = require('./searchRouter')
+const testRouter = require('./testRouter') //test
 
 function route(app) {
+    app.use('/test', testRouter) //test
+
     app.use('/search', searchRouter)
     app.use('/order', orderRouter)
     app.use('/auth', authRouter)
