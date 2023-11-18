52888da31fa569bf1811d8600bfd7d2a21fa5080
[Demo] <[pnhaiyen520@gmail.com]>
2023-11-18 13:58:48 +0700

    add /pages/accout/warranty-claim

--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -7,5 +7,6 @@ const AccountController = require('../controllers/AccountController')
 router.get('/information', AccountController.information)
 router.get('/purchase', AccountController.purchaseHistory)
 router.get('/purchase/:purchaseID', AccountController.purchaseDetail)
+router.get('/warranty-claim', AccountController.warrantyClaim)
 
 module.exports = router
