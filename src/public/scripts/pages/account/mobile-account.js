a89638a12692204a66483ba85dba4dce6dea54dd
namtuthien <leducmanh123kt@gmail.com>
2023-11-30 15:23:41 +0700

    Add js for /site/category-mobile

--- src/public/scripts/pages/account/mobile-account.js
+++ src/public/scripts/pages/account/mobile-account.js
@@ -1,55 +1,55 @@
 // Thêm "active" vào app-bar__element
-const appbar__element = document.querySelectorAll('.app-bar__element');
-appbar__element[3].classList.add('active');
- 
- //sidebar
-  const links = document.querySelectorAll('#sidebar a');
+const appbarEle = document.querySelectorAll('.app-bar__element')
+appbarEle[3].classList.add('active')
 
-  links.forEach(function (link) {
-      link.addEventListener('click', function () {
-          links.forEach(function (otherLink) {
-              otherLink.parentNode.classList.remove('active');
-          });
-  
-          link.parentNode.classList.add('active');
-      });
-  });
-  
-    function redirectTo(url) {
-      window.location.href = url;
+//sidebar
+const links = document.querySelectorAll('#sidebar a')
+
+links.forEach(function (link) {
+    link.addEventListener('click', function () {
+        links.forEach(function (otherLink) {
+            otherLink.parentNode.classList.remove('active')
+        })
+
+        link.parentNode.classList.add('active')
+    })
+})
+
+function redirectTo(url) {
+    window.location.href = url
+}
+
+//Popup
+const modalBtns = document.querySelectorAll('.btn-warranty')
+const modals = document.querySelectorAll('.popup')
+const closeBtns = document.querySelectorAll('.close-btn')
+const cancelBtns = document.querySelectorAll('.btn-cancel')
+
+modalBtns.forEach((btn) => {
+    btn.onclick = function () {
+        modals[0].style.display = 'block'
     }
-  
-  //Popup
-  const modalBtns = document.querySelectorAll('.btn-warranty')
-  const modals = document.querySelectorAll('.popup')
-  const closeBtns = document.querySelectorAll('.close-btn')
-  const cancelBtns = document.querySelectorAll('.btn-cancel')
-  
-  modalBtns.forEach((btn) => {
-      btn.onclick = function () {
-          modals[0].style.display = 'block'
-      }
-  })
-  
-  // Đóng popup khi chọn dấu x
-  closeBtns.forEach((closeBtn) => {
-      closeBtn.onclick = function () {
-          modals[0].style.display = 'none'
-      }
-  })
-  
-  // Đóng popup khi chọn nút hủy
-  cancelBtns.forEach((cancelBtn) => {
-      cancelBtn.onclick = function () {
-          modals[0].style.display = 'none'
-      }
-  })
-  
-  // Đóng popup khi nhấp chuột vào bất kỳ khu vực nào trên màn hình
-  window.onclick = function (e) {
-      modals.forEach((modal) => {
-          if (e.target == modal) {
-              modal.style.display = 'none'
-          }
-      })
-  }
+})
+
+// Đóng popup khi chọn dấu x
+closeBtns.forEach((closeBtn) => {
+    closeBtn.onclick = function () {
+        modals[0].style.display = 'none'
+    }
+})
+
+// Đóng popup khi chọn nút hủy
+cancelBtns.forEach((cancelBtn) => {
+    cancelBtn.onclick = function () {
+        modals[0].style.display = 'none'
+    }
+})
+
+// Đóng popup khi nhấp chuột vào bất kỳ khu vực nào trên màn hình
+window.onclick = function (e) {
+    modals.forEach((modal) => {
+        if (e.target == modal) {
+            modal.style.display = 'none'
+        }
+    })
+}
