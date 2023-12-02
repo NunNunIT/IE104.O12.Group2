b423d194486f2907172ef2faaad6b3f7a6b021b3
LY PHI LAN <21520319@gm.uit.edu.vn>
2023-12-02 15:23:10 +0700

    commit to take hvi branch

--- src/public/scripts/pages/account/warranty-claim.js
+++ src/public/scripts/pages/account/warranty-claim.js
@@ -1,36 +1,35 @@
-// Thêm "active" vào side-menu__ele 
 const appbar__element = document.querySelectorAll('.side-menu__ele');
 appbar__element[2].classList.add('active');
 
 //Popup
-const modalBtns = document.querySelectorAll('.btn-warranty')
-const modals = document.querySelectorAll('.popup')
-const closeBtns = document.querySelectorAll('.close-btn')
-const cancelBtns = document.querySelectorAll('.btn-cancel')
+const openBtn = document.querySelectorAll('.btn-warranty')
+const popup = document.querySelectorAll('.popup')
+const closeBtn = document.querySelectorAll('.close-btn')
+const cancelBtn = document.querySelectorAll('.btn-cancel')
 
-modalBtns.forEach((btn) => {
-    btn.onclick = function () {
-        modals[0].style.display = 'block'
-    }
-})
+// openBtn.forEach((btn) => {
+//     btn.onclick = function () {
+//         popup[0].style.display = 'block'
+//     }
+// })
 
 // Đóng popup khi chọn dấu x
-closeBtns.forEach((closeBtn) => {
+closeBtn.forEach((closeBtn) => {
     closeBtn.onclick = function () {
-        modals[0].style.display = 'none'
+        popup[0].style.display = 'none'
     }
 })
 
 // Đóng popup khi chọn nút hủy
-cancelBtns.forEach((cancelBtn) => {
+cancelBtn.forEach((cancelBtn) => {
     cancelBtn.onclick = function () {
-        modals[0].style.display = 'none'
+        popup[0].style.display = 'none'
     }
 })
 
 // Đóng popup khi nhấp chuột vào bất kỳ khu vực nào trên màn hình
 window.onclick = function (e) {
-    modals.forEach((modal) => {
+    popup.forEach((modal) => {
         if (e.target == modal) {
             modal.style.display = 'none'
         }
