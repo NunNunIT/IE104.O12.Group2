b97691494d5bebb5e62a1e9b9854428abfd764a8
Huong Vi <vithihuong384@gmail.com>
2023-12-02 11:15:59 +0700

    backup FE, add product image P28 - P42, P58

--- src/public/scripts/pages/account/edit-information.js
+++ src/public/scripts/pages/account/edit-information.js
@@ -17,3 +17,51 @@ inputs.forEach(function (input) {
         input.style.color = originalColor;
     });
 });
+
+function checkUserName() {
+    const userNameInput = document.getElementById('userName');
+    const userNameError = document.getElementById('userNameError');
+
+    if (!isValidName(userNameInput.value)) {
+        userNameError.textContent = 'Tên không thể chứa số hoặc các kí tự đặc biệt';
+        userNameError.style.color = 'red';
+    } else {
+        userNameError.textContent = '';
+    }
+}
+
+function checkUserEmail() {
+    const userEmailInput = document.getElementById('userEmail');
+    const userEmailError = document.getElementById('userEmailError');
+
+    if (!isValidEmail(userEmailInput.value)) {
+        userEmailError.textContent = 'Sai định dạng email';
+        userEmailError.style.color = 'red';
+    } else {
+        userEmailError.textContent = '';
+    }
+}
+
+function checkUserPhone() {
+    const userPhoneInput = document.getElementById('userPhone');
+    const userPhoneError = document.getElementById('userPhoneError');
+
+    if (!isValidPhone(userPhoneInput.value)) {
+        userPhoneError.textContent = 'Sai định dạng số điện thoại';
+        userPhoneError.style.color = 'red';
+    } else {
+        userPhoneError.textContent = '';
+    }
+}
+
+function isValidName(name) {
+    return /^[a-zA-Z\s]+$/.test(name);
+}
+
+function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function isValidPhone(phone) {
+    return /^\d{10}$/.test(phone);
+}
