6c98ac7129d8a62e54fd5e86195466be836fc942
Huong Vi <vithihuong384@gmail.com>
2023-12-03 14:12:01 +0700

    fix P24_avt, fix edit-information

--- src/controllers/customer/AuthController.js
+++ src/controllers/customer/AuthController.js
@@ -67,7 +67,7 @@ authController.submitLogin = async (req, res) => {
 
             const cookieOptions = {
                 expires: new Date(
-                    Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 1000
+                    Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
                 ),
                 httpOnly: true,
             }
@@ -82,7 +82,7 @@ authController.submitLogin = async (req, res) => {
 
 authController.logout = async (req, res) => {
     res.cookie('userSave', 'logout', {
-        expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
+        expires: new Date(Date.now() + 2 * 1000),
         httpOnly: true,
     })
     res.status(200).redirect('/')
