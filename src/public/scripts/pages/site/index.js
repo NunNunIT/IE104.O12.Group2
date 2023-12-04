12999410c6efdfee81722939a20f2ad1048cc2cb
namtuthien <leducmanh123kt@gmail.com>
2023-12-05 03:47:13 +0700

    Fix cate carousel in /site/index

--- src/public/scripts/pages/site/index.js
+++ src/public/scripts/pages/site/index.js
@@ -3,9 +3,61 @@ const appbarEle = document.querySelectorAll('.app-bar__element')
 appbarEle[0].classList.add('active')
 
 function calculateDiscountedPrice(originalPrice, discountAmount) {
-    return Math.round(originalPrice - originalPrice * (discountAmount / 100));
+    return Math.round(originalPrice - originalPrice * (discountAmount / 100))
 }
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const carousel = document.getElementById('cateCarousel')
+//     const items = document.querySelectorAll('.cate__carousel-col')
+//     const totalItems = items.length
+//     const maxItemsDisplay = 7
+//     let currentIndex = 0
+
+//     const prevButton = document.querySelector('#catePrev')
+//     const nextButton = document.querySelector('#cateNext')
+//     prevButton.style.display = 'none'
+
+//     if (prevButton && nextButton) {
+//         prevButton.addEventListener('click', showPrev)
+//         nextButton.addEventListener('click', showNext)
+//     }
+
+//     function showPrev() {
+//         if (currentIndex != 0) {
+//             currentIndex = currentIndex - 1
+//             updateCarousel()
+//         }
+
+//         if (window.innerWidth > 416) {
+//             if (currentIndex == 0)
+//                 prevButton.style.display = 'none'
+
+//             if (nextButton.style.display == 'none')
+//                 nextButton.style.display = 'block'
+//         }
+//     }
+
+//     function showNext() {
+//         if ((currentIndex + 1) * maxItemsDisplay < totalItems) {
+//             currentIndex = currentIndex + 1
+//             updateCarousel()
+//         }
+
+//         if (window.innerWidth > 416) {
+//             if (prevButton.style.display == 'none')
+//                 prevButton.style.display = 'block'
+
+//             if ((currentIndex + 1) * maxItemsDisplay >= totalItems) {
+//                 nextButton.style.display = 'none'
+//             }
+//         }
+//     }
+
+//     function updateCarousel() {
+//         const transformValue = -currentIndex * 90 + '%'
+//         carousel.style.transform = `translateX(${transformValue})`
+//     }
+// })
 document.addEventListener('DOMContentLoaded', function () {
     const carousel = document.getElementById('cateCarousel')
     const items = document.querySelectorAll('.cate__carousel-col')
@@ -23,17 +75,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function showPrev() {
-        if (currentIndex != 0) {
+        if (currentIndex !== 0) {
             currentIndex = currentIndex - 1
             updateCarousel()
         }
 
         if (window.innerWidth > 416) {
-            if (currentIndex == 0)
+            if (currentIndex === 0) {
                 prevButton.style.display = 'none'
+            }
 
-            if (nextButton.style.display == 'none')
+            if (nextButton.style.display === 'none') {
                 nextButton.style.display = 'block'
+            }
         }
     }
 
@@ -44,8 +98,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (window.innerWidth > 416) {
-            if (prevButton.style.display == 'none')
+            if (prevButton.style.display === 'none') {
                 prevButton.style.display = 'block'
+            }
 
             if ((currentIndex + 1) * maxItemsDisplay >= totalItems) {
                 nextButton.style.display = 'none'
@@ -54,7 +109,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function updateCarousel() {
-        const transformValue = -currentIndex * 100 + '%'
-        carousel.style.transform = `translateX(${transformValue})`
+        const scrollValue = currentIndex * carousel.clientWidth
+        carousel.scrollTo({
+            left: scrollValue,
+            behavior: 'smooth',
+        })
     }
 })
