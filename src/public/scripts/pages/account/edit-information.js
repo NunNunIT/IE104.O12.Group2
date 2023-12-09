710cba2ab982d70d2728a19162eb3d715be481aa
namtuthien <leducmanh123kt@gmail.com>
2023-12-10 00:04:12 +0700

    Update ejs, js /account/edit-information and /model/account.model

--- src/public/scripts/pages/account/edit-information.js
+++ src/public/scripts/pages/account/edit-information.js
@@ -1,94 +1,143 @@
-const form = document.getElementById('editForm');
-const userName = document.getElementById('userName');
-const userEmail = document.getElementById('userEmail');
-const userPhone = document.getElementById('userPhone');
-const userAddress = document.getElementById('userAddress');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('editForm')
+    const saveButton = document.getElementById('saveButton')
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        validateInput()
+    })
+
+    saveButton.addEventListener('click', function () {
+        validateInput()
+    })
+})
+
+
+const form = document.getElementById('editForm')
+const userName = document.getElementById('userName')
+const userEmail = document.getElementById('userEmail')
+const userPhone = document.getElementById('userPhone')
+const userAddress = document.getElementById('userAddress')
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    validateInput();
-});
+    e.preventDefault()
+    validateInput()
+})
 
 const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error-message');
+    const inputControl = element.parentElement
+    const errorDisplay = inputControl.querySelector('.error-message')
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('error');
-};
+    errorDisplay.innerText = message
+    inputControl.classList.add('error')
+    inputControl.classList.remove('error')
+}
 
 const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error-message');
+    const inputControl = element.parentElement
+    const errorDisplay = inputControl.querySelector('.error-message')
 
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
+    errorDisplay.innerText = ''
+    inputControl.classList.add('success')
+    inputControl.classList.remove('error')
+}
 
 const isValidUserName = userName => {
-    const re = /^[a-zA-Z\sàáạảãăắằẵặẳâầấậẩẫđèéẹẻẽêềếệểễòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữìíịỉĩỳýỵỷỹ]+$/;
-    return re.test(String(userName).trim());
-};
+    const re = /^[a-zA-Z\sàáạảãăắằẵặẳâầấậẩẫđèéẹẻẽêềếệểễòóọỏõôồốộổỗơờớợởỡùúụủũưừứựửữìíịỉĩỳýỵỷỹ]+$/
+    return re.test(String(userName).trim())
+}
 
 const isValidPhoneNumber = phoneNumber => {
-    const re = /^[0-9]{10}$/;
-    return re.test(String(phoneNumber).trim());
-};
+    const re = /^[0-9]{10}$/
+    return re.test(String(phoneNumber).trim())
+}
 
 const isValidEmail = email => {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(String(email).toLowerCase());
-};
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return re.test(String(email).toLowerCase())
+}
 
 const validateInput = () => {
-    const userNameValue = userName.value.trim();
-    const userEmailValue = userEmail.value.trim();
-    const userPhoneValue = userPhone.value.trim();
-    const userAddressValue = userAddress.value.trim();
+    const userNameValue = userName.value.trim()
+    const userEmailValue = userEmail.value.trim()
+    const userPhoneValue = userPhone.value.trim()
+    const userAddressValue = userAddress.value.trim()
 
-    let isAllValid = true;
+    let isAllValid = true
 
     if (userNameValue === '') {
-        setError(userName, 'Vui lòng nhập họ tên!');
-        isAllValid = false;
+        setError(userName, 'Vui lòng nhập họ tên!')
+        isAllValid = false
     } else if (!isValidUserName(userNameValue)) {
-        setError(userName, 'Họ tên không đúng định dạng!');
-        isAllValid = false;
+        setError(userName, 'Họ tên không đúng định dạng!')
+        isAllValid = false
     } else {
-        setSuccess(userName);
+        setSuccess(userName)
     }
 
     if (userEmailValue === '') {
-        setError(userEmail, 'Vui lòng nhập email!');
-        isAllValid = false;
+        setError(userEmail, 'Vui lòng nhập email!')
+        isAllValid = false
     } else if (!isValidEmail(userEmailValue)) {
-        setError(userEmail, 'Email không đúng định dạng!');
-        isAllValid = false;
+        setError(userEmail, 'Email không đúng định dạng!')
+        isAllValid = false
     } else {
-        setSuccess(userEmail);
+        setSuccess(userEmail)
     }
 
     if (userPhoneValue === '') {
-        setError(userPhone, 'Vui lòng nhập số điện thoại!');
-        isAllValid = false;
+        setError(userPhone, 'Vui lòng nhập số điện thoại!')
+        isAllValid = false
     } else if (!isValidPhoneNumber(userPhoneValue)) {
-        setError(userPhone, 'Số điện thoại không đúng định dạng!');
-        isAllValid = false;
+        setError(userPhone, 'Số điện thoại không đúng định dạng!')
+        isAllValid = false
     } else {
-        setSuccess(userPhone);
+        setSuccess(userPhone)
     }
 
     if (userAddressValue === '') {
-        setError(userAddress, 'Vui lòng nhập địa chỉ!');
-        isAllValid = false;
+        setError(userAddress, 'Vui lòng nhập địa chỉ!')
+        isAllValid = false
     } else {
-        setSuccess(userAddress);
+        setSuccess(userAddress)
     }
 
     // Nếu tất cả các trường thông tin hợp lệ, thì gửi form
     if (isAllValid) {
-        form.submit();
+        // form.submit()
+        sendUpdateRequest()
     }
-};
+}
+
+const sendUpdateRequest = async () => {
+    const userNameValue = userName.value.trim()
+    const userBirthValue = userBirth.value.trim()
+    const userSexValue = userSex.value.trim()
+    const userEmailValue = userEmail.value.trim()
+    const userPhoneValue = userPhone.value.trim()
+    const userAddressValue = userAddress.value.trim()
+
+    const requestBody = {
+        user_name: userNameValue,
+        user_birth: userBirthValue,
+        user_sex: userSexValue,
+        user_email: userEmailValue,
+        user_phone: userPhoneValue,
+        user_address: userAddressValue
+    }
+
+    const response = await fetch('/account/edit-information', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+
+    if (response.ok) {
+        console.log('Update successful')
+        window.location.href = '/account/information'
+    } else {
+        console.error('Update failed')
+    }
+}
