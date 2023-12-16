b2710c6d0a6545affdffd3c75340c2f498612bf9
namtuthien <leducmanh123kt@gmail.com>
2023-12-16 17:01:03 +0700

    Fetch data for /account/feedbacks and add success-modal to /account/purchase, /account/changePassword

--- src/public/scripts/pages/account/feedback.js
+++ src/public/scripts/pages/account/feedback.js
@@ -1,50 +1,93 @@
 
-const allStar = document.querySelectorAll('.rating .star');
-const ratingValue = document.querySelector('.rating input');
-const ratingText = document.getElementById('ratingText');
+const allStar = document.querySelectorAll('.rating .star')
+const ratingText = document.getElementById('ratingText')
 
 allStar.forEach((item, idx) => {
-    item.addEventListener('click', function () {
-        let click = 0;
-        ratingValue.value = idx + 1;
+    item.addEventListener('click', function (event) {
+        let current = event.currentTarget
+        let ratingInput = current.parentElement.querySelector('input')
+
+        let click = 0
+        ratingInput.value = idx % 5 + 1
 
         allStar.forEach(i => {
-            i.classList.replace('bxs-star', 'bx-star');
-            i.classList.remove('active');
-        });
+            i.classList.replace('bxs-star', 'bx-star')
+            i.classList.remove('active')
+        })
 
         for (let i = 0; i < allStar.length; i++) {
             if (i <= idx) {
-                allStar[i].classList.replace('bx-star', 'bxs-star');
-                allStar[i].classList.add('active');
+                allStar[i].classList.replace('bx-star', 'bxs-star')
+                allStar[i].classList.add('active')
             } else {
-                allStar[i].style.setProperty('--i', click);
-                click++;
+                allStar[i].style.setProperty('--i', click)
+                click++
             }
         }
 
         // add status of rating stars
         switch (idx + 1) {
             case 1:
-                ratingText.textContent = 'Tệ';
-                break;
+                ratingText.textContent = 'Tệ'
+                break
             case 2:
-                ratingText.textContent = 'Không hài lòng';
-                break;
+                ratingText.textContent = 'Không hài lòng'
+                break
             case 3:
-                ratingText.textContent = 'Bình thường';
-                break;
+                ratingText.textContent = 'Bình thường'
+                break
             case 4:
-                ratingText.textContent = 'Hài lòng';
-                break;
+                ratingText.textContent = 'Hài lòng'
+                break
             case 5:
-                ratingText.textContent = 'Tuyệt vời';
-                break;
+                ratingText.textContent = 'Tuyệt vời'
+                break
             default:
-                ratingText.textContent = '';
+                ratingText.textContent = ''
         }
 
         // add color for rating text
-        ratingText.style.color = '#e5ad06';
-    });
-});
+        ratingText.style.color = '#e5ad06'
+    })
+})
+
+// Fetch rating
+const ratingForm = document.querySelector('#rating-form')
+ratingForm.addEventListener('submit', submitRating)
+function submitRating(event) {
+    event.preventDefault()
+
+    const feedbacks = []
+    const products = Array.from(ratingForm.querySelectorAll('.feedback-product'))
+
+    products.forEach(product => {
+        let productVariantId = product.querySelector('input[name="product_variant_id"]').value
+        let feedbackRate = product.querySelector('input[name="rating"]').value ?? 5
+        let feedbackContent = product.querySelector('textarea').value
+
+        feedbacks.push({
+            product_variant_id: productVariantId,
+            feedback_rate: feedbackRate,
+            feedback_content: feedbackContent,
+        })
+    })
+
+    const orderId = ratingForm.querySelector('input[name="order_id"]').value
+
+    fetch('/account/feedback', {
+        body: JSON.stringify({ order_id: orderId, feedbacks: feedbacks }),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(() => {
+            const successModal = document.querySelector('.success-modal')
+            successModal.style.display = 'flex'
+            setTimeout(() => {
+                successModal.style.display = 'none'
+            }, 1000)
+
+            window.location.href = '/account/purchase'
+        })
+}
--- src/public/scripts/pages/account/purchaseHistory.js
+++ src/public/scripts/pages/account/purchaseHistory.js
@@ -1,92 +1,92 @@
 // Thêm "active" vào side-menu__ele 
-const appbar__element = document.querySelectorAll('.side-menu__ele');
-appbar__element[1].classList.add('active');
+const appbar__element = document.querySelectorAll('.side-menu__ele')
+appbar__element[1].classList.add('active')
 
 // Định nghĩa hàm để thêm class cho các phần tử purchase-item__status và purchase-item__status-container
 function addStatusClasses() {
     // Lấy tất cả các phần tử có class "purchase-item__status-container"
-    let statusContainers = document.querySelectorAll(".purchase-item__status-container");
+    let statusContainers = document.querySelectorAll(".purchase-item__status-container")
 
     // Lặp qua mỗi phần tử và thực hiện kiểm tra và thêm class
     statusContainers.forEach(function (container) {
-        let statusElement = container.querySelector(".purchase-item__status");
-        let orderStatus = statusElement.textContent.trim();
+        let statusElement = container.querySelector(".purchase-item__status")
+        let orderStatus = statusElement.textContent.trim()
 
         // Xóa tất cả các class cũ để tránh việc chúng tích tụ
-        container.classList.remove("cancel", "unpaid", "finished", "delivering");
+        container.classList.remove("cancel", "unpaid", "finished", "delivering")
 
         if (orderStatus === "Đã hủy") {
-            container.classList.add("cancel");
+            container.classList.add("cancel")
         } else if (orderStatus === "Chờ thanh toán") {
-            container.classList.add("unpaid");
+            container.classList.add("unpaid")
         } else if (orderStatus === "Hoàn thành") {
-            container.classList.add("finished");
+            container.classList.add("finished")
         } else if (orderStatus === "Đang giao hàng") {
-            container.classList.add("delivering");
+            container.classList.add("delivering")
         }
-    });
+    })
 }
 
 // Gọi hàm khi DOM đã được tải
 document.addEventListener("DOMContentLoaded", function () {
-    addStatusClasses();
-});
+    addStatusClasses()
+})
 
 document.addEventListener("DOMContentLoaded", function () {
     // Lấy tất cả các liên kết trong thẻ nav có class "purchase__status"
-    var statusLinks = document.querySelectorAll(".purchase__status a");
+    var statusLinks = document.querySelectorAll(".purchase__status a")
 
     // Xác định URL hiện tại và giải mã
-    var currentUrl = decodeURIComponent(window.location.href);
+    var currentUrl = decodeURIComponent(window.location.href)
 
     // Cập nhật class "active" dựa trên URL hiện tại
-    updateActiveLink(statusLinks, currentUrl);
+    updateActiveLink(statusLinks, currentUrl)
 
     // Thêm sự kiện cho sự thay đổi trạng thái trang (như khi chuyển trang)
     window.addEventListener("popstate", function () {
         // Xác định URL mới và giải mã
-        var newUrl = decodeURIComponent(window.location.href);
+        var newUrl = decodeURIComponent(window.location.href)
 
         // Cập nhật class "active" dựa trên URL mới
-        updateActiveLink(statusLinks, newUrl);
-    });
-});
+        updateActiveLink(statusLinks, newUrl)
+    })
+})
 
 // Hàm để cập nhật class "active" dựa trên URL
 function updateActiveLink(links, url) {
     for (var i = 0; i < links.length; i++) {
-        var link = links[i];
-        var linkUrl = decodeURIComponent(link.href);
+        var link = links[i]
+        var linkUrl = decodeURIComponent(link.href)
 
         if (url === linkUrl) {
             // Nếu liên kết có URL trùng khớp với URL hiện tại, thêm class "active"
-            link.classList.add("active");
+            link.classList.add("active")
         } else {
             // Nếu không phải liên kết được chọn, loại bỏ class "active"
-            link.classList.remove("active");
+            link.classList.remove("active")
         }
     }
 }
 
 // Thêm sự kiện click cho nút hủy mua hàng
-const purchaseCancelBtns = document.querySelectorAll(".btn.btn--outlined.pri.cancel");
-const purchaseCancelModals = document.querySelectorAll(".purchase-cancel__popup");
-const cancelConfirmBtns = document.querySelectorAll(".btn.btn--filled.pri.confirm-cancel");
-const purchaseCancelCloseBtns = document.querySelectorAll(".purchase-cancel__popup .close-btn");
-const purchaseCancelConfirmBtns = document.querySelectorAll(".btn.btn--outlined.pri.cancel-purchase");
-const cancelForms = document.querySelectorAll('.his_cancel_popup');
+const purchaseCancelBtns = document.querySelectorAll(".btn.btn--outlined.pri.cancel")
+const purchaseCancelModals = document.querySelectorAll(".purchase-cancel__popup")
+const cancelConfirmBtns = document.querySelectorAll(".btn.btn--filled.pri.confirm-cancel")
+const purchaseCancelCloseBtns = document.querySelectorAll(".purchase-cancel__popup .close-btn")
+const purchaseCancelConfirmBtns = document.querySelectorAll(".btn.btn--outlined.pri.cancel-purchase")
+const cancelForms = document.querySelectorAll('.his_cancel_popup')
 
 
 cancelForms.forEach(form => {
     form.addEventListener('submit', e => {
-        e.preventDefault();
+        e.preventDefault()
     })
 })
 
 
 purchaseCancelBtns.forEach((cancelBtn, index) => {
     cancelBtn.addEventListener("click", function () {
-        purchaseCancelModals[index].style.display = "block";
+        purchaseCancelModals[index].style.display = "block"
         const order_id = {
             order_id: purchaseCancelModals[index].querySelector(`input[name=order_id]`).value
         }
@@ -97,90 +97,45 @@ purchaseCancelBtns.forEach((cancelBtn, index) => {
                 headers: {
                     "Content-Type": "application/json"
                 }
-            }).then(res => res.json())
+            })
+                .then(res => res.json())
                 .then(back => {
                     if (back.status == "error") {
-                        // Hiển thị thông báo vui lòng thử lại sau
-                    } else if (back.status == "success") {
-                        // Hiển thị thông báo đã hủy thành công
-                        // purchaseCancelModals[index].style.display = "none";
-                        //Thay đổi trạng thái
-                        // const purchaseItem = document.querySelectorAll('.purchase-item')[index];
-                        // const statusBlock = purchaseItem.querySelector('.purchase-item__status-container');
-                        // const statusIcon = purchaseItem.querySelector('.material-symbols-outlined.status');
-                        // const status = purchaseItem.querySelector('.purchase-item__status');
-                        // statusBlock.classList.remove("cancel", "unpaid", "finished", "delivering");
-                        // statusBlock.classList.add("cancel");
-                        // statusIcon.innerHTML = "cancel";
-                        // status.innerHTML = "Đã hủy";
-                        // cancelBtn.style.display = "none";
-                        location.reload();
+                        window.alert('Vui lòng thử lại sau')
                     }
-                });
-        });
-    });
-});
+                    else if (back.status == "success") {
+                        const successModal = document.querySelector('.success-modal')
+                        successModal.style.display = 'flex'
+                        setTimeout(() => {
+                            successModal.style.display = 'none'
+                        }, 1000)
+
+                        location.reload()
+                    }
+                })
+        })
+    })
+})
 
 // Đóng popup khi chọn dấu x
 purchaseCancelCloseBtns.forEach((closeBtn, index) => {
     closeBtn.addEventListener("click", function () {
-        purchaseCancelModals[index].style.display = "none";
-    });
-});
+        purchaseCancelModals[index].style.display = "none"
+    })
+})
 
 // Đóng popup khi chọn nút hủy
 purchaseCancelConfirmBtns.forEach((closeBtn, index) => {
     closeBtn.addEventListener("click", function () {
-        purchaseCancelModals[index].style.display = "none";
-    });
-});
+        purchaseCancelModals[index].style.display = "none"
+    })
+})
 
 // Đóng popup khi nhấp chuột vào bất kỳ khu vực nào trên màn hình
 window.addEventListener("click", function (e) {
     purchaseCancelModals.forEach((modal) => {
         if (e.target == modal) {
-            modal.style.display = "none";
+            modal.style.display = "none"
         }
-    });
-});
-
-
-// cancelForms.forEach(cancelForm => {
-//     cancelForm.addEventListener('submit', e => {
-//         // Ngăn chặn việc gửi form nếu có bất kỳ trường nào không hợp lệ
-//         e.preventDefault();
-
-//         // Kiểm tra dữ liệu nhập vào
-
-
-//         fetch('/order/cancel_order', {
-//             method: 'POST',
-//             body: JSON.stringify(order_id),
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         }).then(res => res.json())
-//             .then(back => {
-//                 if (back.status == "error") {
-//                     // Hiển thị thông báo vui lòng thử lại sau
-//                 } else if (back.status == "success") {
-//                     // Hiển thị thông báo đã hủy thành công
-//                     location.reload();
-//                 }
-//             });
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    })
+})
