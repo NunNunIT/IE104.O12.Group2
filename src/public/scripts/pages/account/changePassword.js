d8173c8e2454db745171428b06e9c65464938978
LY PHI LAN <21520319@gm.uit.edu.vn>
2023-12-16 15:34:26 +0700

    fix changePassword mobile

--- src/public/scripts/pages/account/changePassword.js
+++ src/public/scripts/pages/account/changePassword.js
@@ -1,6 +1,9 @@
 const inputs = document.querySelectorAll(".reset__input-field");
 const toggle_btn = document.querySelectorAll(".reset__toggle");
 
+const appbar__element = document.querySelectorAll('.side-menu__ele');
+appbar__element[3].classList.add('active');
+
 inputs.forEach((inp) => {
 	inp.addEventListener("focus", () => {
 		inp.classList.add("active");
